6688ea0f7147f4fc1aee080b5d1e7701292cbc8c
didoshotev <didoshotev@gmail.com>
2022-01-03 17:18:32 +0200

    feat: liquidity pool script

---
+++ pancakeSwap-liquidity/migrations/2_deploy-liquidity-pool.js
@@ -0,0 +1,43 @@
+const Factory = artifacts.require('Factory.sol');
+const Router = artifacts.require('Router.sol');
+const Pair = artifacts.require('Pair.sol');
+const Token1 = artifacts.require('Token1.sol');
+const Token2 = artifacts.require('Token2.sol');
+
+const PANCAKESWAP_CONTRACTS_TESTNET = { }
+PANCAKESWAP_CONTRACTS_TESTNET.FACTORY = '0x6725F303b657a9451d8BA641348b6761A6CC7a17';
+PANCAKESWAP_CONTRACTS_TESTNET.ROUTER = '0xD99D1c33F9fC3444f8101754aBC46c52416550D1';
+
+module.exports = async function (deployer, network, accounts) {
+    const [admin, _] = await web3.eth.getAccounts();
+    try {
+        const factory = await Factory.at(PANCAKESWAP_CONTRACTS_TESTNET.FACTORY); 
+        const router = await Router.at(PANCAKESWAP_CONTRACTS_TESTNET.ROUTER);
+        
+        // might be already deployed
+        const token1 = await Token1.new();
+        const token2 = await Token2.new();
+        // read only transaction so we know the address in advance
+        const pairAddress = await factory.createPair.call(token1.address, token2.address);
+        const tx = await factory.createPair(token1.address, token2.address);
+
+        // provide initial liquidity
+        await token1.approve(router.address, 10000);
+        await token2.approve(router.address, 10000);
+        await router.addLiquidity(
+            token1.address,
+            token2.address,
+            10000,
+            10000,
+            10000,
+            10000,
+            admin,
+            Math.floor(Date.now() / 1000) + 60 * 10
+        );
+        const pair = await Pair.at(pairAddress);
+        const balance = await pair.balanceOf(admin);
+        console.log(`balance LP: ${balance.toString()}`);
+    } catch (e) {
+        console.log(e);
+    }
+};
--- pancakeSwap-liquidity/truffle-config.js
+++ pancakeSwap-liquidity/truffle-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config()
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = process.env.MNEMONIC;
+
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -71,6 +75,14 @@ module.exports = {
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
+    bscTestnet: {
+      provider: () => new HDWalletProvider(
+        mnemonic,
+        'https://data-seed-prebsc-1-s1.binance.org:8545'
+      ),
+      network_id: 97,
+      skipDryRun: true
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -104,13 +116,13 @@ module.exports = {
   // $ truffle migrate --reset --compile-all
   //
   // db: {
-    // enabled: false,
-    // host: "127.0.0.1",
-    // adapter: {
-    //   name: "sqlite",
-    //   settings: {
-    //     directory: ".db"
-    //   }
-    // }
+  // enabled: false,
+  // host: "127.0.0.1",
+  // adapter: {
+  //   name: "sqlite",
+  //   settings: {
+  //     directory: ".db"
+  //   }
+  // }
   // }
 };
